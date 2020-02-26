6b59fec4376f7aea6e51df31754ac69d65b42c23
jason <jason@robotis.com>
2020-02-26 16:10:39 +0900

    determine USER_FLASH_START according to SD version

--- src/usb/uf2/uf2cfg.h
+++ src/usb/uf2/uf2cfg.h
@@ -1,4 +1,5 @@
 #include "boards.h"
+#include "nrf_sdm.h"
 
 #define UF2_NUM_BLOCKS     8000   // at least 4,1 MB for FAT16
 
@@ -9,7 +10,13 @@
 #define FLASH_SIZE         (USER_FLASH_END-USER_FLASH_START) // Max flash size
 
 // Only allow to write application TODO dynamic depending on SD size
+#if SD_VERSION >= 7000000 && SD_VERSION < 8000000
+#define USER_FLASH_START   0x27000
+#elif SD_VERSION >= 6000000 && SD_VERSION < 7000000
 #define USER_FLASH_START   0x26000
+#else
+#error "Unsupported version of SoftDevice!"
+#endif
 #define USER_FLASH_END     0xAD000 // Fat Fs start here
 
 #define FLASH_PAGE_SIZE    4096
